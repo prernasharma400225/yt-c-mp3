dea10e25395f3b9e07f33582d3d0bb01e8e33207
Prerna Sharma <prernasharma0925@gmail.com>
2026-03-19 15:14:23 +0530

    Initial commit with project structure setup

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 connectDB();
 
 app.get("/", (req, res) => {
-  res.send("API is running 🚀");
+  res.send("API is running");
 });
 
 app.use("/api/download", downloadRouter);
--- Backend/src/services/ytdlp.service.js
+++ Backend/src/services/ytdlp.service.js
@@ -1,21 +1,20 @@
 import ytDlp from "yt-dlp-exec";
-import path from "path";
 
 export const convertToMP3 = async (url) => {
-  try {
-    const fileName = `audio-${Date.now()}.mp3`;
-
+  try{
+    const filename = `audio-${Date.now()}.mp3`;
     await ytDlp(url, {
-      format: "bestaudio",
-      output: fileName,
       extractAudio: true,
       audioFormat: "mp3",
-      ffmpegLocation: "C:\\Users\\presh\\Downloads\\ffmpeg-8.1-essentials_build\\ffmpeg-8.1-essentials_build\\bin",
+      output: filename,
+      noCheckCertificates: true,
+      preferFreeFormats: true,
     });
-
-    return path.resolve(fileName);
-  } catch (error) {
-    console.error("❌ ERROR:", error);
-    throw error;
+    return filename;
   }
-};
+  catch (error) {
+    console.error("❌ yt-dlp error:", error);
+    throw error;
+
+  }
+}
